7d15de23368fdb977453b303eb73182f1686984f
turncoda <144861042+turncoda@users.noreply.github.com>
2024-07-21 21:55:31 -0700

    Per-tile shadows

--- main.js
+++ main.js
@@ -40,18 +40,110 @@ async function main() {
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 
-  let source = document.querySelector("#vs").innerHTML.trim();
+  let vs_tiles = document.querySelector("#vs_tiles").innerHTML.trim();
+  let fs_tiles = document.querySelector("#fs_tiles").innerHTML.trim();
+  let sprog_tiles = createProgram(gl, vs_tiles, fs_tiles);
+
+  let vs_shadow = document.querySelector("#vs_shadow").innerHTML.trim();
+  let fs_shadow = document.querySelector("#fs_shadow").innerHTML.trim();
+  let sprog_shadow = createProgram(gl, vs_shadow, fs_shadow);
+
+  {
+    gl.enableVertexAttribArray(0);
+    gl.enableVertexAttribArray(1);
+    let buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+      0.0, 0.0, 0.0, 0.0,
+      1.0, 0.0, 1.0, 0.0,
+      1.0, 1.0, 1.0, 1.0,
+      0.0, 1.0, 0.0, 1.0,
+    ]), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 16, 0);
+    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 16, 8);
+  }
+
+  // --- RENDER ---
+
+  gl.clearColor(0.5, 0.5, 0.5, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  {
+    gl.useProgram(sprog_shadow);
+
+    let u_tex = gl.getUniformLocation(sprog_shadow, "tex");
+    let u_texSize = gl.getUniformLocation(sprog_shadow, "texSize");
+    let u_screenSize = gl.getUniformLocation(sprog_shadow, "screenSize");
+    let u_srcRect = gl.getUniformLocation(sprog_shadow, "srcRect");
+    let u_dstRect = gl.getUniformLocation(sprog_shadow, "dstRect");
+    let u_origin = gl.getUniformLocation(sprog_shadow, "origin");
+
+    gl.uniform1i(u_tex, 0);
+    gl.uniform2f(u_texSize, img_spritesheet.width, img_spritesheet.height);
+    gl.uniform2f(u_screenSize, 320, 180);
+
+    for (const level of ldtk_map.levels) {
+      for (const layer of level.layerInstances) {
+        for (const tile of layer.gridTiles) {
+          gl.uniform4f(u_srcRect,
+            tile.src[0], tile.src[1], layer.__cWid, layer.__cHei);
+          gl.uniform4f(u_dstRect,
+            tile.px[0], tile.px[1], layer.__cWid, layer.__cHei);
+          gl.uniform2f(u_origin,
+            tile.px[0] + 0.5 * (layer.__cWid + layer.__pxTotalOffsetX),
+            tile.px[1] - 0.5 * (layer.__cHei - layer.__pxTotalOffsetY));
+          gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        }
+        console.log(layer.__identifier);
+      }
+    }
+  }
+
+  {
+    gl.useProgram(sprog_tiles);
+
+    let u_tex = gl.getUniformLocation(sprog_tiles, "tex");
+    let u_texSize = gl.getUniformLocation(sprog_tiles, "texSize");
+    let u_screenSize = gl.getUniformLocation(sprog_tiles, "screenSize");
+    let u_srcRect = gl.getUniformLocation(sprog_tiles, "srcRect");
+    let u_dstRect = gl.getUniformLocation(sprog_tiles, "dstRect");
+
+    gl.uniform1i(u_tex, 0);
+    gl.uniform2f(u_texSize, img_spritesheet.width, img_spritesheet.height);
+    gl.uniform2f(u_screenSize, 320, 180);
+
+    for (const level of ldtk_map.levels) {
+      let reversed = level.layerInstances.slice().reverse();
+      for (const layer of reversed) {
+        for (const tile of layer.gridTiles) {
+          gl.uniform4f(u_srcRect,
+            tile.src[0], tile.src[1], layer.__cWid, layer.__cHei);
+          gl.uniform4f(u_dstRect,
+            tile.px[0] + layer.__pxTotalOffsetX, tile.px[1] + layer.__pxTotalOffsetY, layer.__cWid, layer.__cHei);
+          gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        }
+        console.log(layer.__identifier);
+      }
+    }
+  }
+}
+
+window.addEventListener("load", main);
+
+// --- HELPERS ---
+
+function createProgram(gl, vs_source, fs_source) {
   const vs = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vs, source);
+  gl.shaderSource(vs, vs_source);
   gl.compileShader(vs);
   if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
     console.error(gl.getShaderInfoLog(vs));
     return;
   }
 
-  source = document.querySelector("#fs").innerHTML.trim();
   const fs = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fs, source);
+  gl.shaderSource(fs, fs_source);
   gl.compileShader(fs);
   if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
     console.error(gl.getShaderInfoLog(fs));
@@ -70,50 +162,7 @@ async function main() {
     console.error(gl.getProgramInfoLog(program));
     return;
   }
-  let u_tex = gl.getUniformLocation(program, "tex");
-  let u_texSize = gl.getUniformLocation(program, "texSize");
-  let u_screenSize = gl.getUniformLocation(program, "screenSize");
-  let u_srcRect = gl.getUniformLocation(program, "srcRect");
-  let u_dstRect = gl.getUniformLocation(program, "dstRect");
 
-  {
-    gl.enableVertexAttribArray(0);
-    gl.enableVertexAttribArray(1);
-    let buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-      0.0, 0.0, 0.0, 0.0,
-      1.0, 0.0, 1.0, 0.0,
-      1.0, 1.0, 1.0, 1.0,
-      0.0, 1.0, 0.0, 1.0,
-    ]), gl.STATIC_DRAW);
-
-    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 16, 0);
-    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 16, 8);
-  }
-
-  gl.clearColor(0.5, 0.5, 0.5, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.useProgram(program);
-  gl.uniform1i(u_tex, 0);
-  gl.uniform2f(u_texSize, 128, 64);
-  gl.uniform2f(u_screenSize, 320, 180);
-
-  for (const level of ldtk_map.levels) {
-    level.layerInstances.reverse();
-    for (const layer of level.layerInstances) {
-      for (const tile of layer.gridTiles) {
-        gl.uniform4f(u_srcRect,
-          tile.src[0], tile.src[1], layer.__cWid, layer.__cHei);
-        gl.uniform4f(u_dstRect,
-          tile.px[0] + layer.__pxTotalOffsetX, tile.px[1] + layer.__pxTotalOffsetY, layer.__cWid, layer.__cHei);
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-      }
-      console.log(layer.__identifier);
-    }
-  }
-
+  return program;
 }
-
-window.addEventListener("load", main);
 
